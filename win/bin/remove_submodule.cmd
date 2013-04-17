31adccba88a0af87c03cef27b9d645923b8478c7
Victor Safronovich <vsafronovich@gmail.com>
2013-04-17 22:06:31 +0600

    added remove_submodule for git submodule remove

---
+++ win/bin/remove_submodule.cmd
@@ -0,0 +1,9 @@
+set path_to_submodule=%1
+git config -f .gitmodules --remove-section submodule.%path_to_submodule%
+git config -f .git/config --remove-section submodule.%path_to_submodule%
+git rm --cached %path_to_submodule%
+git add .gitmodules
+git commit -m "Remove submodule %path_to_submodule%"
+rm -rf %path_to_submodule%
+rm -rf .git/modules/%path_to_submodule%
+git gc
