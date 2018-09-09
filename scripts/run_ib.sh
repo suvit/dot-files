483c6cbd68c5d9bddccf449f7ce282acc7b119dd
Victor Safronovich <vsafronovich@gmail.com>
2018-09-09 22:48:19 +0500

    added more scripts more docs to todo

---
+++ scripts/run_ib.sh
@@ -0,0 +1,1 @@
+env WINEPREFIX=~/.wine32 LANG=ru_RU.UTF-8 wine ~/crypt/Suvit/banks/NV4/start.exe
---
+++ scripts/run_suvitserver.sh
@@ -0,0 +1,1 @@
+rdesktop 192.168.1.20 -u vsafronovich -K -r clipboard:PRIMARYCLIPBOARD -r disk:sharename=/home/viktor/Old
