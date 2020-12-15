dd761f67f4cee7690ab0b5129aff135bb335c8ef
Victor Safronovich <vsafronovich@gmail.com>
2020-12-15 11:48:25 +0500

    added flags to scripts

--- scripts/odoo/run_odoo.sh
+++ scripts/odoo/run_odoo.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+set -Eeuxo pipefail
+
 pipenv run odoo $@ -c local.conf
--- scripts/odoo/run_test.sh
+++ scripts/odoo/run_test.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+set -Eeuxo pipefail
+
 script_dir=$(dirname "$BASH_SOURCE")
 GITLAB_CI=1 $script_dir/run_odoo.sh $@ --xmlrpc-port 8999 --stop-after-init
