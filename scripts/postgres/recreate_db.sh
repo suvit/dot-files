7a6fafd3899861137b13a74ce65eb8d3f4736550
Victor Safronovich <vsafronovich@gmail.com>
2020-12-24 16:47:00 +0500

    more complex solution to import dumps

--- scripts/postgres/recreate_db.sh
+++ scripts/postgres/recreate_db.sh
@@ -5,12 +5,13 @@ export PGHOST=localhost
 export PGPORT=65432
 DATE=${1:-`date '+%Y-%m-%d'`}
 DB=$2
+DB_FULL=${3:-suvit_dump_pgsql_$DB_11.0_$DATE.gz}
 
 dropdb --if-exists ${DB}
-createdb -O odoo11 ${DB}
+createdb -O $USER ${DB}
 # TODO var date
-# gunzip -c suvit_dump_pgsql_${DB}_11.0_${DATE}.gz | ./pgdump_10_to_9.py | psql -d 11_mrp > /dev/null
-gunzip -c suvit_dump_pgsql_${DB}_11.0_${DATE}.gz | nice psql -d ${DB} > /dev/null
+# gunzip -c ${DB_FULL} | ./pgdump_10_to_9.py | psql -d ${DB} > /dev/null
+gunzip -c ${DB_FULL} | nice psql -d ${DB} > /dev/null
 psql -d ${DB} -c "delete from ir_mail_server where name != 'localhost';"
 psql -d ${DB} -c "delete from ir_config_parameter where key LIKE 'SENTRY%';"
 psql -d ${DB} -c "update ir_cron set active = False"
